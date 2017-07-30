3c77ad7333ddfa87a467b3447544b190a88d6984
Toby Sullivan <tobyjsullivan@gmail.com>
2017-07-29 19:02:39 -0700

    Add check for postgres readiness

--- start-and-run-migrations.sh
+++ start-and-run-migrations.sh
@@ -1,4 +1,11 @@
-sleep 5 && \
-flyway -locations=filesystem:/sql -url=jdbc:postgresql://localhost:5432/postgres -user=postgres -password=$POSTGRES_PASSWORD migrate &
+for i in 1 2 3 4 5 6
+do
+  pg_isready -h localhost -U postgres && \
+  echo "Postgres ready. Migrating..." && \
+  flyway -locations=filesystem:/sql -url=jdbc:postgresql://localhost:5432/postgres -user=postgres -password=$POSTGRES_PASSWORD migrate && \
+  break
+  echo "Postgres not ready"
+  sleep 5
+done &
 docker-entrypoint.sh postgres 
 
